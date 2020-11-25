c2f303ef55beb326424dab5c554ba6b2bff5b2b3
llss1989 <argenkasymov2@gmail.com>
2020-11-26 02:48:16 +0300

    just push

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const genDiff = (firstConfig, secondConfig) => {
   const supportedDataOfFirstFile = getParseData(dataOfFirstFile, typeOfFirstFile);
   const supportedDataOfSecondFile = getParseData(dataOfSecondFile, typeOfSecondFile);
   const keysOfDataOfFirstFile = Object.keys(supportedDataOfFirstFile);
+  return supportedDataOfFirstFile;
   const keyOfDataOfSecondFile = Object.keys(supportedDataOfSecondFile);
   const compareResult = _.union(keysOfDataOfFirstFile, keyOfDataOfSecondFile)
     .sort()
@@ -46,3 +47,43 @@ const genDiff = (firstConfig, secondConfig) => {
   return compareResult.join('\n');
 };
 export default genDiff;
+
+const buildAst = (firstConfig, secondConfig) => {
+  const [dataOfFirstFile, typeOfFirstFile] = getData(firstConfig);
+  const [dataOfSecondFile, typeOfSecondFile] = getData(secondConfig);
+  const supportedDataOfFirstFile = getParseData(dataOfFirstFile, typeOfFirstFile);
+  const supportedDataOfSecondFile = getParseData(dataOfSecondFile, typeOfSecondFile);
+  const iter = (nodeFromFirstFile, nodeFromSecondFile) => {
+    const keysOfDataOfFirstFile = Object.keys(nodeFromFirstFile);
+    const keyOfDataOfSecondFile = Object.keys(nodeFromSecondFile);
+    const ast = _.union(keysOfDataOfFirstFile, keyOfDataOfSecondFile)
+      .sort()
+      .reduce((acc, currentKey) => {
+        //console.log(nodeFromFirstFile[currentKey]);
+        if (typeof (nodeFromFirstFile[currentKey]) !== 'object' || typeof (nodeFromSecondFile[currentKey]) !== 'object') {
+          acc[currentKey] = {
+            key: currentKey,
+            was: {
+              type: typeof (nodeFromFirstFile[currentKey]),
+              value: nodeFromFirstFile[currentKey],
+            },
+            became: {
+              type: typeof (nodeFromSecondFile[currentKey]),
+              value: nodeFromSecondFile[currentKey],
+            },
+          };
+          return acc;
+        }
+        if (typeof (nodeFromFirstFile[currentKey]) === 'object' && typeof (nodeFromSecondFile[currentKey]) === 'object') {
+          return iter(nodeFromFirstFile[currentKey], nodeFromSecondFile[currentKey]);
+        }
+      },
+      {});
+    return ast;
+  };
+  return iter(supportedDataOfFirstFile, supportedDataOfSecondFile);
+};
+
+console.log(buildAst('../__fixtures__/packageRecursive.json', '../__fixtures__/packageRecursive2.yaml'), null, '\t');
+//console.log(buildAst('../__fixtures__/package.json', '../__fixtures__/package2.json'));
+
