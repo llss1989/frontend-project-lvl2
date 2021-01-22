f620f17c69fa799769a01dd983d39f3cf90b1c91
llss1989 <argenkasymov2@gmail.com>
2021-01-22 20:45:51 +0300

    check34

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,7 +2,8 @@ const getValue = (valueKey, depth) => {
   const currentIndent = '  '.repeat(depth * 2);
   const closeBracketIndent = '  '.repeat(depth * 2 - 2);
   if (typeof (valueKey) !== 'object' || valueKey === null) {
-    return `${valueKey}`;
+    const returnedValue = valueKey === '' ? '' : valueKey;
+    return returnedValue;
   }
   const lines = Object.entries(valueKey).map(([key, currentValue]) => `${currentIndent}${key}: ${getValue(currentValue, depth + 1)}`);
   return ['{',
