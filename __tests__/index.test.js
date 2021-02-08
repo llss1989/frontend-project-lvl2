a381d24e67dc4e51f1db41957b52af3e75a7d6a6
llss1989 <argenkasymov2@gmail.com>
2021-02-09 00:57:10 +0300

    deleted fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,22 +6,15 @@ const getPathOfFile = (nameOfFile) => path.resolve(process.cwd(), '__fixtures__'
 const getResult = (filepath) => fs.readFileSync(filepath, 'utf8');
 
 test('basic', () => {
-  expect(genDiff(getPathOfFile('package.json'), getPathOfFile('package2.json'))).toEqual(getResult(getPathOfFile('expected')));
-});
-test('basic-yaml', () => {
-  expect(genDiff(getPathOfFile('package.yml'), getPathOfFile('package2.yml'))).toEqual(getResult(getPathOfFile('expected')));
-});
-
-test('recursive', () => {
-  expect(genDiff(getPathOfFile('packageRecursive.json'), getPathOfFile('packageRecursive2.json'))).toEqual(getResult(getPathOfFile('recursiveExpected')));
+  expect(genDiff(getPathOfFile('before.json'), getPathOfFile('after.json'))).toEqual(getResult(getPathOfFile('stylishExpected')));
 });
 test('plain', () => {
-  expect(genDiff(getPathOfFile('packageRecursive.json'), getPathOfFile('packageRecursive2.json'), 'plain')).toEqual(getResult(getPathOfFile('plainExpected')));
+  expect(genDiff(getPathOfFile('before.json'), getPathOfFile('after.json'), 'plain')).toEqual(getResult(getPathOfFile('plainExpected')));
 });
 test('json', () => {
-  expect(() => JSON.parse(genDiff(getPathOfFile('packageRecursive.json'), getPathOfFile('packageRecursive2.json'), 'json'))).not.toThrow();
+  expect(() => JSON.parse(genDiff(getPathOfFile('before.json'), getPathOfFile('after.json'), 'json'))).not.toThrow();
 });
 
-test('recursive-stylish', () => {
-  expect(genDiff(getPathOfFile('packageRecursive.yaml'), getPathOfFile('packageRecursive2.json'), 'stylish')).toEqual(getResult(getPathOfFile('recursiveExpected')));
+test('stylish', () => {
+  expect(genDiff(getPathOfFile('before.yml'), getPathOfFile('after.yml'))).toEqual(getResult(getPathOfFile('stylishExpected')));
 });
