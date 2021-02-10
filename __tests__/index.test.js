a0e4db4f28d4472b44097785eabec852625ae8ca
llss1989 <argenkasymov2@gmail.com>
2021-02-10 11:49:07 +0300

    tests change

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,13 +8,16 @@ const getResult = (filepath) => fs.readFileSync(filepath, 'utf8');
 test('basic', () => {
   expect(genDiff(getPathOfFile('before.json'), getPathOfFile('after.json'))).toEqual(getResult(getPathOfFile('stylishExpected')));
 });
-test('plain', () => {
-  expect(genDiff(getPathOfFile('before.yml'), getPathOfFile('after.json'), 'plain')).toEqual(getResult(getPathOfFile('plainExpected')));
-});
 test('json', () => {
   expect(() => JSON.parse(genDiff(getPathOfFile('before.json'), getPathOfFile('after.json'), 'json'))).not.toThrow();
 });
+const cases = [['before.json', 'after.json', 'stylish'], ['before.yml', 'after.json', 'plain'], ['before.yml', 'after.json', 'plain']];
 
-test('stylish', () => {
-  expect(genDiff(getPathOfFile('before.yml'), getPathOfFile('after.yml'), 'stylish')).toEqual(getResult(getPathOfFile('stylishExpected')));
+describe('add utility', () => {
+  test.each(cases)(
+    'test different formatteres',
+    (firstFile, secondFile, formatter) => {
+      expect(genDiff(getPathOfFile(firstFile), getPathOfFile(secondFile), formatter)).toEqual(getResult(getPathOfFile(`${formatter}Expected`)));
+    },
+  );
 });
