7011269eb4002e44784dfd0094b3cf6d046f47b1
llss1989 <argenkasymov2@gmail.com>
2021-04-11 23:13:36 +0300

    refactor plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,21 +8,19 @@ const getValueForPlain = (value) => {
 };
 const parseCurrentNode = (currentNode, keyPath, iter) => {
   const newKeyPath = keyPath !== '' ? `${keyPath}.${currentNode.nameOfKey}` : `${currentNode.nameOfKey}`;
-  if (currentNode.childrens.length === 0) {
-    if (currentNode.status === 'added') {
-      return `Property '${newKeyPath}' was added with value: ${getValueForPlain(currentNode.value)}`;
-    }
-    if (currentNode.status === 'deleted') {
-      return `Property '${newKeyPath}' was removed`;
-    }
-    if (currentNode.status === 'updated') {
-      return `Property '${newKeyPath}' was updated. From ${getValueForPlain(currentNode.value[0])} to ${getValueForPlain(currentNode.value[1])}`;
-    }
-  }
   if (currentNode.childrens.length > 0) {
     return iter(currentNode.childrens, `${newKeyPath}`);
   }
+  if (currentNode.childrens.length === 0 && currentNode.status !== 'no_changed'){
+    return parseCurrentNode.states[currentNode['status']](newKeyPath, currentNode);
+  }
 };
+parseCurrentNode.states = {
+  added: (newKeyPath, currentNode) => `Property '${newKeyPath}' was added with value: ${getValueForPlain(currentNode.value)}`,
+  deleted: (newKeyPath, currentNode) => `Property '${newKeyPath}' was removed`,
+  updated: (newKeyPath, currentNode) => `Property '${newKeyPath}' was updated. From ${getValueForPlain(currentNode.value[0])} to ${getValueForPlain(currentNode.value[1])}`,
+}
+
 const plain = (ast) => {
   const iter = (tree, keyPath) => {
     const lines = tree.map((currentNode) => parseCurrentNode(currentNode, keyPath, iter));
