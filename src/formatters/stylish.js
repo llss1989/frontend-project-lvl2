be536c8b7a3959a9a9a18696e24eaa0593b1229a
llss1989 <argenkasymov2@gmail.com>
2021-04-12 02:43:15 +0300

    back

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -12,31 +12,35 @@ const getValue = (valueKey, depth) => {
     ...lines,
     `${closeBracketIndent}}`].join('\n');
 };
-const parseCurrentNode = (node, iter) => {
-  // console.log(node)
-  const currentIndent = node.status === undefined ? '  '.repeat((node.depth * 2)) : '  '.repeat((node.depth * 2) - 1);
-  if (node.childrens.length !== 0) {
-    return `\n${currentIndent}${node.nameOfKey}: {${iter(node.childrens)}\n${currentIndent}}`;
-  }
-  return parseCurrentNode.states[node.status](node, currentIndent);
-};
-parseCurrentNode.states = {
-  added: (currentNode, currentIndent) => `\n${currentIndent}+ ${currentNode.nameOfKey}: ${getValue(currentNode.value, currentNode.depth + 1)}`,
-  deleted: (currentNode, currentIndent) => `\n${currentIndent}- ${currentNode.nameOfKey}: ${getValue(currentNode.value, currentNode.depth + 1)}`,
-  no_changed: (currentNode, currentIndent) => `\n${currentIndent}  ${currentNode.nameOfKey}: ${getValue(currentNode.value, currentNode.depth + 1)}`,
-  updated: (currentNode, currentIndent) => `\n${currentIndent}- ${currentNode.nameOfKey}: ${getValue(currentNode.value[0], currentNode.depth + 1)}
-${currentIndent}+ ${currentNode.nameOfKey}: ${getValue(currentNode.value[1], currentNode.depth + 1)}`,
-
-};
 const stylish = (ast) => {
   const iter = (tree) => {
-    // console.log(`${JSON.stringify(tree, null, ' ')}XXXXXXXXXXX`);
-    const lines = tree.map((node) => parseCurrentNode(node, iter));
-    // console.log(lines)
+    const lines = tree.reduce((acc, node) => {
+      const currentIndent = node.status === undefined ? '  '.repeat((node.depth * 2)) : '  '.repeat((node.depth * 2) - 1);
+      if (node.childrens.length === 0) {
+        if (node.status === 'added') {
+          acc.push(`${currentIndent}+ ${node.nameOfKey}: ${getValue(node.value, node.depth + 1)}`);
+        }
+        if (node.status === 'deleted') {
+          acc.push(`${currentIndent}- ${node.nameOfKey}: ${getValue(node.value, node.depth + 1)}`);
+        }
+        if (node.status === 'no_changed') {
+          acc.push(`${currentIndent}  ${node.nameOfKey}: ${getValue(node.value, node.depth + 1)}`);
+        }
+        if (node.status === 'updated') {
+          acc.push(`${currentIndent}- ${node.nameOfKey}: ${getValue(node.value[0], node.depth + 1)}`);
+          acc.push(`${currentIndent}+ ${node.nameOfKey}: ${getValue(node.value[1], node.depth + 1)}`);
+        }
+      }
+      if (node.childrens.length !== 0) {
+        acc.push(`${currentIndent}${node.nameOfKey}: {`);
+        acc.push(iter(node.childrens));
+        acc.push(`${currentIndent}}`);
+      }
+      return acc.flat(1);
+    }, []);
     return lines;
   };
-  const result = iter(ast).filter((x) => typeof(x) !== '');
-  // console.log(`${result}!!!!!!!!!!!!1`)
+  const result = iter(ast);
   return [
     '{',
     ...result,
